4e20505e239ab642409144226022db8755021d31
Megatens <53794600+Megatens@users.noreply.github.com>
2025-01-12 14:07:27 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,17 +1,10 @@
 def get_matrix(n, m, value):
-    # Создаем пустой список для матрицы
     matrix = []
-    # Внешний цикл для строк
     for i in range(n):
-        # Создаем пустой список для строки
         row = []
-        # Внутренний цикл для столбцов
-        for j in range(m):
-            # Заполняем строку значениями value
+         for j in range(m):
             row.append(value)
-        # Добавляем заполненную строку в матрицу
         matrix.append(row)
-    # Возвращаем созданную матрицу
     return matrix
 print(get_matrix(2, 2, 10))
 print(get_matrix(3, 5, 42))
